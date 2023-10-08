03ec16d5d018dc4c677fef7097669f1a1c00c98d
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-10-08 12:20:16 +0100

    add feat tabs for tessting

--- App.tsx
+++ App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import { Home } from "./screens/";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+
+import { BookDetail } from "./screens/";
+import Tabs from "./navigation/tabs";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent"
+  }
+}
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-      <View style={styles.container}>
-        <Home />
-      </View>
-  );
-};
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Home'}
+      >
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
+
+        {/* Screens */}
+        <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
