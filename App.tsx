6cb83996d3923eb6a98d01b19cb07cc8330674f1
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-10-17 10:56:31 +0100

    add naimation

--- App.tsx
+++ App.tsx
@@ -5,16 +5,21 @@ import { View, Text , Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Navigation from './src/components/Navigation/Navigation';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
+    <GestureHandlerRootView style={{ flex: 1 }} >
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Navigation" component={Navigation} />
       </Stack.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+      
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,90 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
 
-function HomeScreen({ navigation }: {navigation:any}) {
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
+const SIZE = 80;
+const CIRCLE_RADIUS = SIZE * 2;
+
+
+function HomeScreen() {
+
+    const translationX = useSharedValue(0);
+    const translationY = useSharedValue(0);
+
+    const panGestureEvent = useAnimatedGestureHandler({
+        onStart: (event : any, context :any) => {
+            context.translateX = translationX.value;
+            context.translateY = translationY.value;
+        },
+        onActive: (event: any, context: any ) => {
+            translationX.value = event.translationX + context.translateX;
+            translationY.value = event.translationY + context.translateY;
+        },
+        onEnd: () => {
+            translationX.value = withSpring(0);
+            translationY.value = withSpring(0);
+        },
+    });
+
+    const rStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: translationX.value },
+                { translateY: translationY.value }
+            ]
+        };
+    });
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Details"
-                onPress={() => {
-                    /* 1. Navigate to the Details route with params */
-                    navigation.navigate('Details', {
-                        itemId: 86,
-                        otherParam: 'anything you want here',
-                    });
-                }}
-            />
+        <View style={styles.container}>
+            <View style={styles.circle}>
+                <PanGestureHandler onGestureEvent={panGestureEvent} >
+                    <Animated.View style={[styles.square, rStyle]} />
+                </PanGestureHandler>
+            </View>
         </View>
+   
     );
 }
 
-
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        textAlign: 'center',
+        justifyContent: 'center' 
+    },
+    square: {
+        height: SIZE,
+        width: SIZE,
+        backgroundColor: "tomato",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20
+    },
+    circle: {
+        height: CIRCLE_RADIUS * 2,
+        width: CIRCLE_RADIUS * 2,
+        borderRadius: CIRCLE_RADIUS,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 4,
+        borderColor: "tomato",
+    }
+
+});
+
+
+
+
+
+
+// const PangestureHandler = () => {
+
+
+//     );
+// }
